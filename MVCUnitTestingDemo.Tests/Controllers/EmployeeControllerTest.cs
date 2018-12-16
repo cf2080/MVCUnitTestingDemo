0ff0d67b4ca29d2683b240a5652911b1b69860c6
cf2080 <cf2080@gmail.com>
2018-12-15 20:02:23 -0800

    Add project files.

---
+++ MVCUnitTestingDemo.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCUnitTestingDemo.Controllers;
+
+namespace MVCUnitTestingDemo.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public void Employees()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
